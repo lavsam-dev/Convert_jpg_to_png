0bd5c868afb19517935863dcb532310b9a1cb330
lavsam-dev <lavsam@gmail.com>
2021-10-27 23:38:52 +0400

    Initial commit

---
+++ app/src/main/java/lavsam/gb/libs/poplibs/l4/ImageConverter.kt
@@ -0,0 +1,34 @@
+package lavsam.gb.libs.poplibs.l4
+
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import io.reactivex.rxjava3.core.Single
+import java.io.FileOutputStream
+
+object ImageConverter {
+
+    fun convertJpgToPng(bitmap: Bitmap, pathToBitmap: String): Single<Pair<String, Bitmap>> {
+        val (pathImagePickedDir, nameImagePicked) = splitPathToBitmap(pathToBitmap)
+        return Single.fromCallable {
+            val pathImageOutput = "$pathImagePickedDir/$nameImagePicked.png"
+            val imageOutputStream = FileOutputStream(pathImageOutput)
+
+            if (bitmap.compress(Bitmap.CompressFormat.PNG, 100, imageOutputStream)) {
+                return@fromCallable (pathImageOutput to BitmapFactory.decodeFile(pathImageOutput))
+            } else {
+                throw Exception("Conversion problem")
+            }
+        }
+    }
+
+    private fun splitPathToBitmap(pathToBitmap: String): Pair<String, String> {
+        val pathImagePickedParts = pathToBitmap.split("/")
+        val pathImagePickedDir = pathImagePickedParts
+            .subList(1, pathImagePickedParts.size - 1)
+            .joinToString(prefix = "/", separator = "/")
+        val nameImagePicked = pathImagePickedParts[pathImagePickedParts.size - 1]
+            .split(".")[0]
+
+        return pathImagePickedDir to nameImagePicked
+    }
+}
